51580f4450e3fbb508b0f4b9072564e9eb4e68ca
swampfox76 <swamp.fox076@gmail.com>
2016-03-26 17:55:47 -0500

    began making the homepage dynamic

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,6 +7,8 @@ class EmployeesController < ApplicationController
 
     def create
         @employee = Employee.new(employee_params)
+        current_user.employees << @employee
+        current_user.save
         if @employee.save
             redirect_to home_path(@employee)
         else
@@ -16,7 +18,15 @@ class EmployeesController < ApplicationController
     end
 
     def show
-
+        id = params[:id]
+        @employees_list = current_user.employees
+        if @employees_list.empty?
+            redirect_to addemployee_path
+        elsif id == "0"
+            @employee = @employees_list.first
+        else
+            @employee = Employee.find(id)
+        end
     end
 
 private

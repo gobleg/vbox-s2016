3987e6b6c323496dd7e2e62821323a62a2ec726d
swampfox76 <swamp.fox076@gmail.com>
2016-04-20 00:11:18 -0500

    added video upload and display

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,24 +9,30 @@ class LocationsController < ApplicationController
         require 'date'
 
         #logger.debug "params: #{params}"
-        locations_files = params[:locations]
+        input_files = params[:inputFiles]
         @employee = Employee.where("name = ? AND eid = ?", params[:employee][:name], params[:employee][:eid]).take
         if @employee.nil?
             flash[:alert] = "Employee not found"
             redirect_to upload_path
-        elsif params[:locations].nil?
+        elsif params[:inputFiles].nil?
             flash[:alert] = "No file selected"
             redirect_to upload_path
         else
-            for file in locations_files
-                #logger.debug "file_path: #{file.path}"
-                if File.extname(file.path) == ".loc"
-                    CSV.foreach(file.path) do |row|
+            logger.debug "#{input_files}"
+            for file in input_files
+                logger.debug "test"
+                if File.extname(file["attributes"].path) == ".loc"
+                    CSV.foreach(file["attributes"].path) do |row|
                         location = Location.new(lat: row[0], lng: row[1], time: Time.at(Integer(row[2])).to_datetime)
                         @employee.locations << location
                         @employee.save
                         location.save
                     end
+                elsif File.extname(file["attributes"].path) == ".mp4"
+                    video = Video.create(:dash_video => file["attributes"])
+                    @employee.videos << video
+                    @employee.save
+                    video.save
                 end
             end
             redirect_to home_path(@employee, 0)
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ActiveRecord::Base
   has_attached_file :dash_video
+  validates_attachment_content_type :dash_video, :content_type => "video/mp4"
   belongs_to :employee
 end

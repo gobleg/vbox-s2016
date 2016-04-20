2f34bd41f9ca905052ada45d86d7cf5b52aa8152
swampfox76 <swamp.fox076@gmail.com>
2016-04-20 16:49:44 -0500

    cleaned up duplicate code

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -25,45 +25,28 @@ class EmployeesController < ApplicationController
             redirect_to addemployee_path
         elsif id == "0"
             @employee = @employees_list.first
-            all_locations = @employee.locations
-            if date == "0"
-                all_dates = all_locations.map { |location| location['time'] }
-                most_recent_date = all_dates.max
-                if most_recent_date.nil?
-                    most_recent_date = Time.now
-                end
-                @display_date = most_recent_date.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")
-                @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", most_recent_date.beginning_of_day, most_recent_date.end_of_day, @employee.id).all
-            else
-                date_p = DateTime.strptime(date, "%b %d, %Y")
-                @display_date = date
-                @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", date_p.beginning_of_day, date_p.end_of_day, @employee.id).all
-            end
-            @times = @locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%I:%M:%S %p %b %d, %Y") }
-            @dates = all_locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y") }
-            @dates = @dates.uniq
         else
             @employee = Employee.find(id)
-            all_locations = @employee.locations
-            if date == "0"
-                all_dates = all_locations.map { |location| location['time'] }
-                most_recent_date = all_dates.max
-                if most_recent_date.nil?
-                    most_recent_date = Time.now
-                end
-                @display_date = most_recent_date.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")
-                @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", most_recent_date.beginning_of_day, most_recent_date.end_of_day, @employee.id).all
-            else
-                date_p = DateTime.strptime(date, "%b %d, %Y")
-                @display_date = date
-                @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", date_p.beginning_of_day, date_p.end_of_day, @employee.id).all
+        end
+        all_locations = @employee.locations
+        if date == "0"
+            all_dates = all_locations.map { |location| location['time'] }
+            most_recent_date = all_dates.max
+            if most_recent_date.nil?
+                most_recent_date = Time.now
             end
-            @times = @locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%I:%M:%S %p %b %d, %Y") }
-            @dates = all_locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y") }
-            @dates = @dates.uniq
-            if @employee.user_id != current_user.id
-                redirect_to home_path(0, 0)
-            end
+            @display_date = most_recent_date.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")
+            @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", most_recent_date.beginning_of_day, most_recent_date.end_of_day, @employee.id).all
+        else
+            date_p = DateTime.strptime(date, "%b %d, %Y")
+            @display_date = date
+            @locations = Location.where("time BETWEEN ? AND ? AND employee_id = ?", date_p.beginning_of_day, date_p.end_of_day, @employee.id).all
+        end
+        @times = @locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%I:%M:%S %p %b %d, %Y") }
+        @dates = all_locations.map { |location| location['time'].in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y") }
+        @dates = @dates.uniq
+        if @employee.user_id != current_user.id
+            redirect_to home_path(0, 0)
         end
     end
 
